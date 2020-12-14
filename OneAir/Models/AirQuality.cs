52525316991f0e6206a7dd3e54d772424f55b76a
farukawofeso <1809152@chester.ac.uk>
2020-12-14 20:29:33 +0000

    New Changes

---
+++ OneAir/Models/AirQuality.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OneAir.Models
+{
+    public class AirQuality
+    {
+        public long AirQualityID { get; set; }
+        public Guid DeviceID { get; set; }
+        public DateTime Time { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int PM10 { get; set; }
+        public int PM25 { get; set; }
+    }
+}
---
+++ OneAir/Models/SqlDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace OneAir.Models
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+          : base(options)
+        {
+        }
+        public DbSet<AirQuality> AirQualities { get; set; }
+    }
+}
