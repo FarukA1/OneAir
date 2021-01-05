c5283b36da7ad2ba322e2cbb06f79b5c87ec0b02
FARUK AWOFESO <1809152@chester.ac.uk>
2021-01-05 17:51:20 +0000

    Migration Set up

---
+++ OneAir/Migrations/20210105174313_OneAir.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OneAir.Migrations
+{
+    public partial class OneAir : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AirQualities",
+                columns: table => new
+                {
+                    AirQualityID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeviceID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Latitude = table.Column<double>(type: "float", nullable: false),
+                    Longitude = table.Column<double>(type: "float", nullable: false),
+                    PM10 = table.Column<int>(type: "int", nullable: false),
+                    PM25 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AirQualities", x => x.AirQualityID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AirQualities");
+        }
+    }
+}
