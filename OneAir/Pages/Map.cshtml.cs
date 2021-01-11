b0fea2e11b95609f53a9e4e308e86d624fe725e0
FARUK AWOFESO <1809152@chester.ac.uk>
2021-01-11 19:36:02 +0000

    test map

---
+++ OneAir/Pages/Map.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OneAir.Models;
+
+namespace OneAir.Pages
+{
+    public class MapModel : PageModel
+    {
+        private readonly OneAir.Models.SqlDbContext _context;
+
+        public MapModel(OneAir.Models.SqlDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<AirQuality> AirQuality { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            AirQuality = await _context.AirQualities.ToListAsync();
+        }
+    }
+}
